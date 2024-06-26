191a1dbafd6b92d7691b13b8d1b0986cdad96a3a
eBean <yongbo.dengyb@gmail.com>
2024-06-26 23:03:33 +0800

    lab2 hidden test 2nd try

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -7,13 +7,14 @@ package DebugExercise;
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+//        int w = (b - a) >> 31;
+//        /* If you're stepping into this function, click the
+//           step out button because you're not going to learn anything. */
+//        int z = ~(b - a) >> 31;
+//
+//        int max = b & w | a & z;
+//        return max;
+        return a > b ? a : b;
     }
 
 
@@ -71,7 +72,8 @@ public class DebugExercise2 {
     public static int sumOfElementwiseMaxes(int[] a, int[] b) {
         int[] maxes = arrayMax(a, b);
         int sumofMaxes = arraySum(maxes);
-        return arraySum(a)+arraySum(b)-sumofMaxes;
+//        return arraySum(a)+arraySum(b)-sumofMaxes;
+        return sumofMaxes;
     }
 
 
