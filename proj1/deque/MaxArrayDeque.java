aa1d6268a604273d478afad2cc268a94d6e3c6dd
eBean <yongbo.dengyb@gmail.com>
2024-08-11 14:34:54 +0800

    finished MaxArray 1.0
    '

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,12 +3,25 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
-  public MaxArrayDeque() {
-    super();
-  }
+  Comparator<T> comp;
   public MaxArrayDeque(Comparator<T> c){
       super();
+      comp = c;
   }
-  public T max(){}
-  public T max(Comparator<T> c){}
+  public T max(){
+    if (isEmpty()) return null;
+    T tmpMax = get(0);
+    for (int i = 1; i < size(); i++){
+      if (comp.compare(tmpMax, get(i)) < 0) tmpMax = get(i);
+    }
+    return tmpMax;
+  }
+  public T max(Comparator<T> c){
+    if (isEmpty()) return null;
+    T tmpMax = get(0);
+    for (int i = 1; i < size(); i++){
+      if (c.compare(tmpMax, get(i)) < 0) tmpMax = get(i);
+    }
+    return tmpMax;
+  }
 }
