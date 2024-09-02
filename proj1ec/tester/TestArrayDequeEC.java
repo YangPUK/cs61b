5f2628e8634c95d7f3e4a0a63581c5e0c955439f
eBean <yongbo.dengyb@gmail.com>
2024-09-02 15:08:56 -0700

    finished proj1EC 3nd

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -10,9 +10,10 @@ import java.util.ArrayDeque;
 public class TestArrayDequeEC {
     @Test
     public void randomizedTest() {
-        int N = 20;
+        int N = 50;
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        String ErrMessage = "";
         for (int i = 0; i < N; i++) {
             int opNum = StdRandom.uniform(0,5);
             if (opNum == 0) {
@@ -20,36 +21,37 @@ public class TestArrayDequeEC {
                 int valNum = StdRandom.uniform(0,100);
                 ads.addFirst(valNum);
                 sad.addFirst(valNum);
-                System.out.println("addFirst(" + valNum + ")");
-                assertEquals("call addFirst",ads.get(0), sad.get(0));
+                ErrMessage += "addFirst(" + valNum + ")\n";
+                assertEquals(ErrMessage,ads.get(0), sad.get(0));
             }
             if (opNum == 1) {
                 //addLast
                 int valNum = StdRandom.uniform(0,100);
                 ads.addLast(valNum);
                 sad.addLast(valNum);
-                System.out.println("addLast( " + valNum + ")");
-                assertEquals("call addLast",ads.get(ads.size() - 1), sad.get(sad.size() - 1));
+                ErrMessage += "addLast(" + valNum + ")\n";
+                assertEquals(ErrMessage ,ads.get(ads.size() - 1), sad.get(sad.size() - 1));
             }
             if (opNum == 2) {
                 //removeFirst
                 if (sad.isEmpty() && ads.isEmpty()) {
-                    System.out.println("removeFirst()");
+                    ErrMessage += "removeFirst()\n";
                     continue;
                 }
                 Integer res = ads.removeFirst();
-                System.out.println("removeFirst(): " + res);
-                assertEquals("call removeFirst", res, sad.removeFirst());
+                ErrMessage += "removeFirst(): " + res + "\n";
+                assertEquals(ErrMessage, res, sad.removeFirst());
             }
             if (opNum == 3) {
                 //removeFirst
                 if (sad.isEmpty() && ads.isEmpty()) {
                     System.out.println("removeLast()");
+                    ErrMessage += "removeLast()\n";
                     continue;
                 }
                 Integer res = ads.removeLast();
-                System.out.println("removeLast(): " + res);
-                assertEquals("call removeLast", res, sad.removeLast());
+                ErrMessage += "removeLast(): " + res + "\n";
+                assertEquals(ErrMessage, res, sad.removeLast());
             }
         }
     }
