c5cef1b305d894a2dffa3b6061f3d27cd27014a4
eBean <yongbo.dengyb@gmail.com>
2024-09-07 14:26:53 -0700

    done with lab7 1st

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,6 +1,8 @@
 package bstmap;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Set;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
@@ -19,8 +21,8 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
             this.value = value;
             this.parent = parent;
         }
-    }
 
+    }
     public BSTMap() {
         size = 0;
         root = null;
@@ -35,12 +37,13 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         if (node == null) {
             return false;
         }
-        if (key.compareTo(node.key) == 0) {
+        else if (key.compareTo(node.key) == 0) {
             return true;
         }
         else if (key.compareTo(node.key) < 0) {
             return containsHelper(key, node.left);
-        } else {
+        }
+        else {
             return containsHelper(key, node.right);
         }
     }
@@ -48,22 +51,80 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         return containsHelper(key, root);
     }
 
+    private V getHelper(K key, BSTNode<K, V> node) {
+        if (node == null) {
+            return null;
+        }
+        else if (key.compareTo(node.key) == 0) {
+            return node.value;
+        }
+        else if (key.compareTo(node.key) < 0) {
+            return getHelper(key, node.left);
+        }
+        else {
+            return getHelper(key, node.right);
+        }
+    }
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
+    }
+
+    private void putHelper(K key, V value, BSTNode<K, V> node, BSTNode<K, V> parent, boolean isLeft) {
+        if (node == null) {
+            if (isLeft) {
+                parent.left = new BSTNode(key, value, null, null, parent);
+            }
+            else {
+                parent.right = new BSTNode(key, value, null, null, parent);
+            }
+            this.size += 1;
+        }
+        else if(key.compareTo(node.key) == 0) {
+            node.value = value;
+        }
+        else if (key.compareTo(node.key) < 0) {
+            putHelper(key, value, node.left, node, true);
+        }
+        else {
+            putHelper(key, value, node.right, node, false);
+        }
+    }
+    public void put(K key, V value) {
+        if (size() == 0) {
+            this.root = new BSTNode(key, value, null, null, null);
+            this.size += 1;
+            return;
+        }
+        putHelper(key, value, root, root, true);
     }
 
     public int size() {
         return size;
     }
 
-    public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+    private String printHelper(BSTNode<K, V> node) {
+        if (node == null) {
+            return "";
+        }
+        return printHelper(node.left) + "\n" + node.key.toString() + "\n" + printHelper(node.right);
     }
-
     public void printInOrder() {
-        return;
+        String s;
+        s = printHelper(root);
+        System.out.println(s.toString());
     }
 
+
+    private BSTNode<K, V> getFirst() {
+        BSTNode<K, V> node = root;
+        if (node == null) {
+            return null;
+        }
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
 
     // Exception
     public Set<K> keySet() {
